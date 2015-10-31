444de1585621d66e095a64e96496e20cb3d2b8a7
Himanshu Mishra <himanshu.m786@gmail.com>
2015-11-01 01:30:42 +0530

    making changes

---
+++ autoupdate.sh
@@ -0,0 +1,7 @@
+echo 'adding changes files ------'
+git add --all
+echo  'ading commit message ------'
+git commit -m 'making changes'
+echo 'pushing up the codes ------'
+git push origin master
+echo 'Code updated. -------------'
